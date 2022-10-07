00dfd7d4b100c9c3c859697c2b16fa36bc56fb54
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-10-07 16:49:41 +0000

    Punto2

--- prepVerifica/src/app/app-routing.module.ts
+++ prepVerifica/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'product/:id', component: ProductComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
